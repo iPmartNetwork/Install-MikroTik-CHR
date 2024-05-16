accb78010f5ce37f7c3ecbb43c41e4e89b3d535a
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-16 13:27:23 +0330

    Update mik7143ARM64.sh

--- mik7143ARM64.sh
+++ mik7143ARM64.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-apt update
-apt install -y pwgen coreutils unzip
-echo
 echo "=== iPmart.Shop ==="
 echo "
 ____________________________________________________________________________________
@@ -24,26 +21,33 @@ sleep 3
 ADDRESS=`ip addr show enp0s3 | grep global | cut -d' ' -f 6 | head -n 1`
 #GATEWAY=xxx.xxx.xxx.1
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3`
-
-wget https://download.mikrotik.com/routeros/7.14.3/chr-7.14.3.img.zip -O chr.img.zip  && \
-gunzip -c chr.img.zip > chr.img  && \
+# Raw disk image https://mikrotik.com/download#chr
+#VERSIONCHR=7.10.2
+VERSIONCHR=7.14.3
+ 
+apt update
+apt install -y pwgen coreutils unzip
+wget -4 https://download.mikrotik.com/routeros/$VERSIONCHR/chr-$VERSIONCHR.img.zip -O chr.img.zip
+gunzip -c chr.img.zip > chr.img
 mount -o loop,offset=33571840 chr.img /mnt
+PASSWORD=$(pwgen 12 1)
 echo "Username: admin"
-echo "Password: admin"
+echo "Password: $PASSWORD"
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]" > /mnt/rw/autorun.scr
 echo "/ip route add gateway=$GATEWAY" >> /mnt/rw/autorun.scr
 echo "/ip service disable telnet" >> /mnt/rw/autorun.scr
-echo "/user set 0 name=admin password=admin" >> /mnt/rw/autorun.scr
+echo "/user set 0 name=admin password=$PASSWORD" >> /mnt/rw/autorun.scr
 echo "/ip dns set server=8.8.8.8,1.1.1.1" >> /mnt/rw/autorun.scr
 # remount all mounted filesystems to read-only mode
 echo u > /proc/sysrq-trigger
-dd if=chr.img bs=1024 of=/dev/sda
+dd if=chr.img bs=1024 of=/dev/vda
 echo "sync disk"
 # synchronize all mounted filesystems
 echo s > /proc/sysrq-trigger
 echo "Sleep 10 seconds"
 #/usr/bin/sleep 10
 read -t 10 -u 1
+echo "Ok, reboot"
 echo "
 ____________________________________________________________________________________
         ____                             _     _                                     
@@ -53,6 +57,5 @@ ________________________________________________________________________________
 _/___/________/_/__/_(___(_/_____(_ __/___|/____(___ _(_ __|/_|/__(___/_/_____/___\__
                                                                                      
 "
-echo "Ok, reboot"
 # perform an immediate OS reboot similar to the RESET button (without synchronising and unmounting file systems)
 echo b > /proc/sysrq-trigger
